8a5956ca4b488cae696314da3ef58c6d212a71fd
j.sadel <kubasadel97@gmail.com>
2020-07-03 14:47:17 +0200

    New controller

--- domain/src/main/java/com/smartphoneShop/backend/controller/SmartphonesController.java
+++ domain/src/main/java/com/smartphoneShop/backend/controller/SmartphonesController.java
@@ -1,4 +1,10 @@
 package com.smartphoneShop.backend.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/smartphones")
 public class SmartphonesController {
 }
