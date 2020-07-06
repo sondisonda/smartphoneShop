c87795922aa03eb587e987c2b2fb349c21c64c86
j.sadel <kubasadel97@gmail.com>
2020-07-06 14:56:16 +0200

    Integrated Angular Material

--- angularSmartphoneShop/src/app/app.module.ts
+++ angularSmartphoneShop/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @NgModule({
@@ -13,7 +15,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
